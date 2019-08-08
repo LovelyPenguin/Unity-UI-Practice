c52b503f6797fc43c35f26e47b34f6be94bea78e
lyp3238 <lyp3238@gmail.com>
2019-08-08 22:18:04 +0900

    완성된 유니티 UI

---
+++ Assets/Script/MousePositionTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePositionTracker : MonoBehaviour
+{
+    public float mouseX;
+    public float mouseY;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ConvertMousePositionToZeroOne();
+    }
+
+    private void ConvertMousePositionToZeroOne()
+    {
+        mouseX = Input.mousePosition.x / Screen.width;
+        mouseY = Input.mousePosition.y / Screen.height;
+    }
+}
---
+++ Assets/Script/ValueCaculrator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueCaculrator : MonoBehaviour
+{
+    public MousePositionTracker manager;
+
+    [SerializeField]
+    private float minPositionX;
+    [SerializeField]
+    private float maxPositionX;
+
+    [Range(0, 1)]
+    public float slider;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Lerp(minPositionX, maxPositionX, manager.mouseX), transform.position.y, transform.position.z);
+    }
+
+    void CaculatePosition()
+    {
+
+    }
+}
